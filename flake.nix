eeca72f50dbb85425b64324c688de84e93c20886
charlie <chwiegand@proton.me>
2024-03-02 02:05:21 +0100

    fix line

--- flake.nix
+++ flake.nix
@@ -110,7 +110,7 @@
                         ];
                     home-manager.backupFileExtension = "bak";
                     }
-                ];;
+                ];
             }
         };
     };
