73107435db51076ae42d7a07f0a166bc459e9e0b
charlie <chwiegand@proton.me>
2024-03-02 02:40:24 +0100

    commit fixes

--- machines/nixos/nixos-server/shares/default.nix
+++ machines/nixos/nixos-server/shares/default.nix
@@ -45,10 +45,10 @@ users.users.charlie.extraGroups = ["share"];
 
 systemd.tmpfiles.rules = map (x: "d ${x.path} 0775 share share - -") (lib.attrValues smb.share_list) ++ ["d /mnt 0775 share share - -"];
 
-system.activationScripts.samba_user_create = ''
-    smb_password=$(cat "${config.age.secrets.sambaPassword.path}")
-    echo -e "$smb_password\n$smb_password\n" | /run/current-system/sw/bin/smbpasswd -a -s share
-    '';
+#system.activationScripts.samba_user_create = ''
+#    smb_password=$(cat "${config.age.secrets.sambaPassword.path}")
+#    echo -e "$smb_password\n$smb_password\n" | /run/current-system/sw/bin/smbpasswd -a -s share
+#    '';
 
 networking.firewall = {
   allowedTCPPorts = [ 5357 ];
@@ -74,29 +74,29 @@ services.samba = {
     '';
   shares = smb_shares;
 };
-services.avahi = {
-  enable = true;
-  nssmdns = true;
-  publish = {
-    enable = true;
-    addresses = true;
-    domain = true;
-    hinfo = true;
-    userServices = true;
-    workstation = true;
-  };
-  extraServiceFiles = {
-    smb = ''
-      <?xml version="1.0" standalone='no'?><!--*-nxml-*-->
-      <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
-      <service-group>
-      <name replace-wildcards="yes">%h</name>
-      <service>
-      <type>_smb._tcp</type>
-      <port>445</port>
-      </service>
-      </service-group>
-      '';
-  };
+#services.avahi = {
+#  enable = true;
+#  nssmdns = true;
+#  publish = {
+#    enable = true;
+#    addresses = true;
+#    domain = true;
+#    hinfo = true;
+#    userServices = true;
+#    workstation = true;
+#  };
+#  extraServiceFiles = {
+#    smb = ''
+#      <?xml version="1.0" standalone='no'?><!--*-nxml-*-->
+#      <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+#      <service-group>
+#      <name replace-wildcards="yes">%h</name>
+#      <service>
+#      <type>_smb._tcp</type>
+#      <port>445</port>
+#      </service>
+#      </service-group>
+#      '';
+#  };
 };
 }
