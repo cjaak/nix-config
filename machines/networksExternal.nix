a4a90266e11a4dc8e6441704a18b8a9fe01daa6d
charlie <chwiegand@proton.me>
2024-03-02 02:46:02 +0100

    commit fixes

--- machines/networksExternal.nix
+++ machines/networksExternal.nix
@@ -1,6 +1,6 @@
 { config, ... }:
 {
-  networking.firewall = {
+  networks.firewall = {
     enable = true;
     allowedTCPPorts = [ 22 80 443 ];  # Example ports for SSH, HTTP, HTTPS
     allowedUDPPorts = [ ];           # Add UDP ports if necessary
--- machines/networksLocal.nix
+++ machines/networksLocal.nix
@@ -1,6 +1,6 @@
 { config, ... }:
 {
-  networking = {
+  networks = {
     hostName = "nixos-server";  # Your server's hostname
     interfaces.eth0.ip4 = [ { address = "192.168.1.2"; prefixLength = 24; } ];
     defaultGateway = "192.168.1.1";
