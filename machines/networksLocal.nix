78cfcd577131258f2947a1c0a92a4011e8571b56
charlie <chwiegand@proton.me>
2024-03-25 18:51:22 +0100

    commit fixes

--- machines/networksLocal.nix
+++ machines/networksLocal.nix
@@ -2,22 +2,25 @@
 {
   networks = {
     lan = {
-      enable = true; # Assuming you want to explicitly enable this network definition
-      interface = "eth0";
-      cidr = "192.168.1.0/24";  # CIDR notation for your LAN
+      enable = true;
+      interface = "eth0";  # Adjust this as per your network interface
+      cidr = "192.168.1.0/24";
 
       reservations = [
         {
-          hostname = "nixos-server"; # Your server's hostname
-          ip-address = "192.168.178.59"; # The desired static IP address for this host
+          hostname = "nixos-server";
+          ip-address = "192.168.178.59";
         }
-        # ... other reservations if needed
+        {
+          hostname = "raspberrypi";
+          ip-address = "192.168.178.44";
+        }
+        # ... other reservations for different hosts
       ];
     };
 
-    # Assuming that defaultGateway and nameservers apply to the LAN
-    defaultGateway = "192.168.1.1";
-    nameservers = [ "192.168.1.1" ];
+    defaultGateway = "192.168.1.1";  # Assuming this is your gateway
+    nameservers = [ "192.168.1.1" ];  # Assuming this is your DNS server
   };
 
   # Additional local network settings can be added here
