0d86c94b0b9fd333774532784b4aa49b8a534e58
charlie <chwiegand@proton.me>
2024-03-25 18:42:09 +0100

    commit fixes

--- machines/networksLocal.nix
+++ machines/networksLocal.nix
@@ -1,10 +1,24 @@
 { config, ... }:
 {
   networks = {
-    hostName = "nixos-server";  # Your server's hostname
-    interfaces.eth0.ip4 = [ { address = "192.168.1.2"; prefixLength = 24; } ];
+    lan = {
+      enable = true; # Assuming you want to explicitly enable this network definition
+      interface = "eth0";
+      cidr = "192.168.1.0/24";  # CIDR notation for your LAN
+
+      reservations = [
+        {
+          hostname = "nixos-server"; # Your server's hostname
+          ip-address = "192.168.178.59"; # The desired static IP address for this host
+        }
+        # ... other reservations if needed
+      ];
+    };
+
+    # Assuming that defaultGateway and nameservers apply to the LAN
     defaultGateway = "192.168.1.1";
     nameservers = [ "192.168.1.1" ];
   };
+
   # Additional local network settings can be added here
 }
