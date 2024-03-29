ad24aa72df9307e6b8e1aa62756ebc060fc84821
charlie <chwiegand@proton.me>
2024-03-29 14:43:29 +0100

    commit fixes

--- services/arr/default.nix
+++ services/arr/default.nix
@@ -16,10 +16,10 @@ directories = [
 
 system.activationScripts.recyclarr_configure = ''
     sed=${pkgs.gnused}/bin/sed
-    configFile=${vars.serviceConfigRoot}/recyclarr/templates/recyclarr.yml
+    configFile=${vars.serviceConfigRoot}/recyclarr/recyclarr.yml
     sonarr="${inputs.recyclarr-configs}/sonarr/templates/web-2160p-v4.yml"
     sonarrApiKey=$(cat "${config.age.secrets.sonarrApiKey.path}")
-    radarr="${inputs.recyclarr-configs}/radarr/remux-web-2160p.yml"
+    radarr="${inputs.recyclarr-configs}/radarr/templates/remux-web-2160p.yml"
     radarrApiKey=$(cat "${config.age.secrets.radarrApiKey.path}")
 
     cat $sonarr > $configFile
