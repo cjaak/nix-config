d8a1136d252db53895ac99bab231df88c32b0e44
charlie <chwiegand@proton.me>
2024-03-30 00:01:40 +0100

    commit fixes

--- machines/nixos/nixos-server/shares/default.nix
+++ machines/nixos/nixos-server/shares/default.nix
@@ -73,29 +73,29 @@ services.samba = {
     '';
   shares = smb_shares;
 };
-#services.avahi = {
-#  enable = true;
-#  nssmdns = true;
-#  publish = {
-#    enable = true;
-#    addresses = true;
-#    domain = true;
-#    hinfo = true;
-#    userServices = true;
-#    workstation = true;
-#  };
-#  extraServiceFiles = {
-#    smb = ''
-#      <?xml version="1.0" standalone='no'?><!--*-nxml-*-->
-#      <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
-#      <service-group>
-#      <name replace-wildcards="yes">%h</name>
-#      <service>
-#      <type>_smb._tcp</type>
-#      <port>445</port>
-#      </service>
-#      </service-group>
-#      '';
-# };
-#};
+services.avahi = {
+  enable = true;
+  nssmdns = true;
+  publish = {
+    enable = true;
+    addresses = true;
+    domain = true;
+    hinfo = true;
+    userServices = true;
+    workstation = true;
+  };
+  extraServiceFiles = {
+    smb = ''
+      <?xml version="1.0" standalone='no'?><!--*-nxml-*-->
+      <!DOCTYPE service-group SYSTEM "avahi-service.dtd">
+      <service-group>
+      <name replace-wildcards="yes">%h</name>
+      <service>
+      <type>_smb._tcp</type>
+      <port>445</port>
+      </service>
+      </service-group>
+      '';
+ };
+};
 }
