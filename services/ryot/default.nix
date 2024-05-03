118d42bd3ca29a919ec1d5ffaedc43c54dff8694
charlie <chwiegand@proton.me>
2024-05-03 20:29:33 +0200

    ryot setup

--- services/ryot/default.nix
+++ services/ryot/default.nix
@@ -10,16 +10,17 @@ in
   # Create directories with proper permissions
   systemd.tmpfiles.rules = map (x: "d ${x} 0777 share share - -") directories;
 
-  # Configure firewall to allow traffic on port 8000
-  networking.firewall.allowedTCPPorts = [ 8000 ];
-
   # Container management via Podman
   virtualisation.oci-containers = {
     containers = {
       ryot-db = {
         image = "docker.io/postgres:16-alpine";
+        autoStart = true;
         volumes = [
           "${vars.serviceConfigRoot}/ryot/data:/var/lib/postgresql/data"
+        ];
+        extraOptions = [
+          "--network=container:ryot"
         ];
         environment = {
           POSTGRES_PASSWORD = "postgres";
@@ -30,10 +31,19 @@ in
 
       ryot = {
         image = "ghcr.io/ignisda/ryot:latest";
-        ports = [
-          "8000:8000"
+        autoStart = true;
+        extraOptions = [
+          "-l=traefik.enable=true"
+          "-l=traefik.http.routers.ryot.rule=Host(`ryot${vars.domainName}`)"
+          "-l=traefik.http.services.paperless.loadbalancer.server.port=8282"
+          "-l=homepage.group=Services"
+          "-l=homepage.name=Ryot"
+          "-l=homepage.icon=ryot.svg"
+          "-l=homepage.href=https://ryot${vars.domainName}"
+          "-l=homepage.description=Ultimate Tracker"
         ];
         environment = {
+          PORT = "8282";
           DATABASE_URL = "postgres://postgres:postgres@ryot-db:5432/postgres";
           # FRONTEND_INSECURE_COOKIES = "true"; # Uncomment if running on HTTP
         };
