77504efc586267748e6107c6e6555bc277630079
charlie <chwiegand@proton.me>
2024-10-08 18:22:24 +0200

    update nixos to 24.05

--- flake.nix
+++ flake.nix
@@ -2,10 +2,10 @@
     description = "my nix home server";
     
     inputs = {
-        nixpkgs.url = "github:nixos/nixpkgs/release-23.11";
-        nixvim.url = "github:pta2002/nixvim/nixos-23.11";
+        nixpkgs.url = "github:nixos/nixpkgs/release-24.05";
+        nixvim.url = "github:pta2002/nixvim/nixos-24.05";
         home-manager = {
-        url = "github:nix-community/home-manager/release-23.11";
+        url = "github:nix-community/home-manager/release-24.05";
         inputs.nixpkgs.follows = "nixpkgs";
         };
         agenix = {
